9106eae3919894478467eaec436b516a37d99ad9
Wong Yiu Hang <henrymail2006@ymail.com>
2018-11-18 21:46:06 +0800

    Fix: Pass exception to parent function

--- Cache.py
+++ Cache.py
@@ -2,9 +2,12 @@ import csv
 import os
 
 def readCache(year, month, day):
-	with open("cache/" + str(int(year)) + "-" + str(int(month)) + "-" + str(int(day)) + ".csv", 'rb') as csvfile:
-		resultList = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
-		return resultList
+	try:
+		with open("cache/" + str(int(year)) + "-" + str(int(month)) + "-" + str(int(day)) + ".csv", 'rb') as csvfile:
+			resultList = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
+			return resultList
+	except IOError as e:
+		raise e
 
 def saveCache(year, month, day, cacheList):
 	if not os.path.exists("./cache/"):
